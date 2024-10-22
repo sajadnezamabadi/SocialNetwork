0e42080f9f5630a5534f6b4b9e4a9cbcdad25f49
unknown <sjdnez9402@gmail.com>
2024-10-22 16:26:36 +0330

    add friendship

--- social_network/urls.py
+++ social_network/urls.py
@@ -33,5 +33,6 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     
-    path("api/",include('posts.urls'))
+    path("api/",include('posts.urls')),
+    path('api/friendship/', include('friendship.urls')),
 ]
